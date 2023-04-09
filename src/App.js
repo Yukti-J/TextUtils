5d2bf19cf8d123484c56c2f79a56b61a028cf2c7
Yukti Jhawar <yuktimj2002@gmail.com>
2023-04-09 08:18:03 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-// import logo from './logo.svg';
 import { useState } from 'react';
 import './App.css';
 import Alerts from './Components/Alerts';
+// import About from './Components/About';
 import Navbar from './Components/Navbar';
 import Textform from './Components/Textform';
+// import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+// import Owl from './images/owl.png';
 
 function App() {
   const [mode,setMode] = useState('light')
   const[text,setText] = useState('Enable Dark Mode')
   const[alert,setAlert] = useState(null)
-
-  const alertSettings = (message, type) => {
+   const alertSettings = (message, type) => {
       setAlert({
         message: message,
         type: type
       })
       setTimeout(() => {
         setAlert(null);
-      }, 2000);
+      }, 2500);
   }
   const toggleMode = () => {
     if(mode==='light'){
@@ -33,11 +34,17 @@ function App() {
     }
   }
   return (
-   <>
-   <Navbar title="TextUtils" link1="Home" link2="About Us" theme={mode} toggleMode={toggleMode} value={text}/>
+<>
+  {/* // <Router>    */}
+    <Navbar title="TextUtils" link1="Home" link2="About Us" theme={mode} toggleMode={toggleMode} value={text}/>
    <Alerts alert={alert} />
-   <Textform theme={mode}/>     
-   </>
+   <Textform theme={mode}/> 
+  {/* //  <Routes>
+  //  <Route exact path="/about" element={<About />} />
+  //  <Route exact path="/" element={ <Textform theme={mode}/> } />
+  //  </Routes>
+  //  </Router> */}
+  </>
   );
 }
 
--- src/Components/About.js
+++ src/Components/About.js
@@ -1,9 +1,11 @@
-import React from 'react'
-
+import React from 'react';
+import Owl from '../images/owl.png';
 export default function About() {
   return (
     <div>
-      
+      <h1 className='m-4'>About Us</h1>
+      <p className='m-5'>In literary theory, a text is any object that can be "read", whether this object is a work of literature, a street sign, an arrangement of buildings on a city block, or styles of clothing. It is a coherent set of signs that transmits some kind of informative message.[1] This set of signs is considered in terms of the informative message's content, rather than in terms of its physical form or the medium in which it is represented.</p>
+     <img className="text-center" src={ Owl} alt='owl' width="20px" height="20px" />
     </div>
   )
 }
